ee9dbaa8fc4e20f40e560287594c4b03413e504f
Syboxez Blank <@Syboxez:matrix.org>
2023-01-12 07:01:03 +0000

    boot/mkinitcpio-kupfer-hooks: Rewrite rootfsresize-hooks.sh to stop hardcoding

--- boot/mkinitcpio-kupfer-hooks/rootfsresize-hooks.sh
+++ boot/mkinitcpio-kupfer-hooks/rootfsresize-hooks.sh
@@ -1,11 +1,24 @@
 #!/usr/bin/ash
 
 run_hook() {
-  if parted -s /dev/loop0 print free | tail -n2 | head -n1 | grep -qi "free space"; then
-    echo "Found unallocated space. Resizing..."
-    parted -s /dev/loop0 resizepart 2 100%
-    partprobe
-    e2fsck -fy /dev/loop0p2
-    resize2fs /dev/loop0p2
+  # rootfsdetect must be run first, or otherwise a root variable must be defined in the boot cmdline
+  if [ ! -n $root ]; then
+    echo "root variable undefined. Aborting..."
+    return 1
   fi
+
+  # Check if root is inside of a nested partition table and fall over to resizing fs if not
+  if [ "$(echo $root | grep /dev/loop)" ]; then
+    _subpartnumber="$(echo $root | grep -Eo '[0-9]+$')"
+    _loopdev="${root%%p$_subpartnumber}"
+    if parted -s "$_loopdev" print free | tail -n2 | head -n1 | grep -qi "free space"; then
+      echo "Found unallocated space on a subpartition. Resizing..."
+      parted -s "$_loopdev" resizepart "$_subpartnumber" 100%
+      partprobe
+    fi
+  fi
+
+  e2fsck -fy "$root"
+  # There is no need to check if filesystem needs to be resized since resize2fs already checks unless "-f" is specified
+  resize2fs "$root"
 }
