89e15dee1eda8ad2f8624f741fcb86a7412bcb5e
Syboxez Blank <@Syboxez:matrix.org>
2023-07-09 12:44:19 -0500

    boot/mkinitcpio-kupfer-hooks/rootfsdetect-hooks.sh: Switch to using exported variables to find loop device

--- boot/mkinitcpio-kupfer-hooks/rootfsresize-hooks.sh
+++ boot/mkinitcpio-kupfer-hooks/rootfsresize-hooks.sh
@@ -8,15 +8,16 @@ run_hook() {
   fi
 
   # Check if root is inside of a nested partition table and fall over to resizing fs if not
-  if [ "$(echo $root | grep /dev/loop)" ]; then
-    _subpartnumber="$(echo $root | grep -Eo '[0-9]+$')"
-    _loopdev="${root%%p$_subpartnumber}"
-    if parted -s "$_loopdev" print free | tail -n2 | head -n1 | grep -qi "free space"; then
+  if [ -n $LOOPDEV ] && [ -n $SUBPARTNUMBER ] ; then
+    if parted -s "$LOOPDEV" print free | tail -n2 | head -n1 | grep -qi "free space"; then
       echo "Found unallocated space on a subpartition. Resizing..."
-      parted -s "$_loopdev" resizepart "$_subpartnumber" 100%
+      parted -s "$LOOPDEV" resizepart "$SUBPARTNUMBER" 100%
       partprobe
     fi
   fi
+
+  unset LOOPDEV
+  unset SUBPARTNUMBER
 
   e2fsck -fy "$root"
   # There is no need to check if filesystem needs to be resized since resize2fs already checks unless "-f" is specified
